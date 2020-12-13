813593fcd0a50a235a15f1b85c3c4935363bda84
Tareeq Hamilton <tareeqhamilton@gmail.com>
2020-12-13 16:40:25 -0500

    added prompts for password

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// create password questions function
+passwordQuestion();
+
+function passwordQuestion() {
+  //prompt for password length
+  var passwordLength = parseInt(prompt("How many characters for your password?"));
+  if (isNaN(passwordLength)===true) {
+    alert("Must enter a number to generate password!!!");
+    return;
+  }
+  if (passwordLength > 128) {
+    alert("Password must be less than 128 characters");
+    return;
+  }
+  if (passwordLength < 8) {
+    alert("Password must be greater than 8 characters");
+    return;
+  }
+
+  //alert with answer
+  
+  //confirms for true or false questions
+  //alert after each 
+}
+
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 
@@ -12,7 +37,7 @@
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
+// var testq = prompt("whats your favorite color?")
 
 // possible paassword values
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
